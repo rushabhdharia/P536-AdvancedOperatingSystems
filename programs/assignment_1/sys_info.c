38a8ca8fad18b859d6a7b16f864decc9fc56cd2f
Rushabh Ashok Dharia <rdharia@silo.soic.indiana.edu>
2018-09-01 18:30:18 -0400

    part 2 date function done

--- programs/assignment_1/sys_info.c
+++ programs/assignment_1/sys_info.c
@@ -3,18 +3,24 @@
 #include <stdio.h>
 #include <string.h>
 #include <errno.h>
-
-#define BUFFER_SIZE = 20
+#include <unistd.h>
 
 int main(int argc, char *argv[])
 {
 	int pid;
-	int p[1];
-	char *command = NULL;	
-	char buffer[BUFFER_SIZE]
+	int p[2];
+	char path[20];	
+	int a = strlen(argv[1]);
+	//printf("%d\n",a);
+	//printf("%d\n",strlen("/bin/date"));
+	
+	if (pipe(p) == -1)
+	{
+		fprintf(stderr, "Pipe Failed, Error no = %d \n", errno);
+		return 1;
+	}
 
 	pid = fork();
-	pipe(x);
 
 	if(pid == -1) // Fork command failed
 	{
@@ -25,20 +31,22 @@ int main(int argc, char *argv[])
 	else if(pid == 0) // Child process
 	{
 		printf("Child PID = %d\n", getpid());
-		read(p[0], buffer, BUFFERSIZE);
+		close(p[1]);
+		read(p[0], path, sizeof(path));
+		printf("%d \t",a);
+		printf("%s \n", path);
+		execl(path, "", 0);
+		close(p[0]);
 	}
 	
 	else // Parent process
 	{
 		printf("Parent PID = %d\n", getpid());
-		//pipe(x);
-		
-		//close(x[0]);
-		write(p[1], argv[1], sizeof(argv[1]));
-			
+		close(p[0]);
+		//printf("%s\n", argv[1]);
+		write(p[1], argv[1], a+1);
+		close(p[1]);
 		wait(NULL); //Wait for child
-		close(x);
-
 	}	
 
 	return 0;
