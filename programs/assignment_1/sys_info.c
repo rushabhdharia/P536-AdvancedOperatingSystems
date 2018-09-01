97b645cf29fb6067cbad3096f4ca79fd271be518
RushabhDharia <rdharia@iu.edu>
2018-09-01 19:57:16 -0400

    Part 2 Code Cleaning

--- programs/assignment_1/sys_info.c
+++ programs/assignment_1/sys_info.c
@@ -8,13 +8,15 @@
 int main(int argc, char *argv[])
 {
 	int pid;
-	int p[2];
-	char path[20];	
-	int a = strlen(argv[1]);
-	//printf("%d\n",a);
-	//printf("%d\n",strlen("/bin/date"));
+	int p[2];	//p[0] - read end of the pipe, p[1] - write end of the pipe
+	char path[20];	//to store the path
 	
-	if (pipe(p) == -1)
+        if(!argv[1]) 	//checks if argument is passed.
+	{
+		printf("Please enter a path. For example = /bin/date\n");
+		return 1;		
+	}
+	if (pipe(p) == -1)	//checks if pipe is created 
 	{
 		fprintf(stderr, "Pipe Failed, Error no = %d \n", errno);
 		return 1;
@@ -22,36 +24,37 @@ int main(int argc, char *argv[])
 
 	pid = fork();
 
-	if(pid == -1) // Fork command failed
+	if(pid == -1) 	// Fork command failed
 	{
-		fprintf(stderr, "Error no = %d\n", errno); // man7.org/linux/man-pages/man3/errno.3.html
+		fprintf(stderr, "Error no = %d\n", errno); 	// man7.org/linux/man-pages/man3/errno.3.html
 		return 1;
 	}
 	
-	else if(pid == 0) // Child process
+	else if(pid == 0) 	// Child process
 	{
 		printf("Child PID = %d\n", getpid());
-		close(p[1]);
+		close(p[1]); 	//close write end of pipe as it is not required
 		read(p[0], path, sizeof(path));
-		//printf("%d \t",a);
-		//printf("%s \n", path);
 		if(strcmp(path,"/bin/echo")==0)
 		{
-			execl(path, "-c", "\"Hello World!\"", "", 0); //https://stackoverflow.com/questions/32771494/how-can-i-use-execl-function-or-other-kinds-of-exec-functions-to-perform-ech/
+			execl(path, "-c", "\"Hello World!\"", "", 0); 	//https://stackoverflow.com/questions/32771494/how-can-i-use-execl-function-or-other-kinds-of-exec-functions-to-perform-ech/
 			fprintf(stderr, "Error no = %d\n", errno);
 		}
 		else
+		{
 			execl(path, "", 0);
-		close(p[0]);
+			perror("Error: "); 
+			fprintf(stderr, "Error no = %d\n", errno);
+		}
+		close(p[0]); 	//close read end
 	}
 	
 	else // Parent process
 	{
 		printf("Parent PID = %d\n", getpid());
-		close(p[0]);
-		//printf("%s\n", argv[1]);
-		write(p[1], argv[1], a+1);
-		close(p[1]);
+		close(p[0]); //close read end of pipe as it is not required
+		write(p[1], argv[1], strlen(argv[1]));
+		close(p[1]); //close write end
 		wait(NULL); //Wait for child
 	}	
 
