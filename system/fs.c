a3b5c7022517673c3344898737a04c5c1500831b
RushabhDharia <rdharia@iu.edu>
2018-11-30 11:28:53 -0500

    Working

--- system/fs.c
+++ system/fs.c
@@ -217,7 +217,8 @@ void fs_printfreemask(void) {
 }
 
 
-int fs_open(char *filename, int flags) {
+int fs_open(char *filename, int flags)
+{	
 	int i, j, success;
 	
 	if(flags != O_RDONLY && flags != O_WRONLY && flags != O_RDWR)
@@ -270,6 +271,7 @@ int fs_open(char *filename, int flags) {
 	}
 
  	return next_open_fd;
+
 }
 
 int fs_close(int fd) {
@@ -295,6 +297,7 @@ int fs_close(int fd) {
 }
 
 int fs_create(char *filename, int mode) {
+
 	int i, success;
 	struct inode *in;
 
@@ -310,6 +313,9 @@ int fs_create(char *filename, int mode) {
 		return SYSERR;
 	}
 	
+	
+	if(fsd.root_dir.numentries != 0)
+	{
 	for(i=0; i<fsd.root_dir.numentries ;i++)
 	{
 		if(strncmp(fsd.root_dir.entry[i].name, filename, FILENAMELEN) == 0)
@@ -318,7 +324,8 @@ int fs_create(char *filename, int mode) {
 			return SYSERR;
 		} 
 	}
-	
+	}
+
 	if(fsd.inodes_used >= fsd.ninodes)
 	{
 		printf("Could not create file as all inodes are used\n");
@@ -340,19 +347,13 @@ int fs_create(char *filename, int mode) {
 	}	
 
 	fsd.root_dir.numentries++;
-	i = fsd.root_dir.numentries - 1;
-	fsd.root_dir.entry[i].inode_num = fsd.inodes_used;
+	i = fsd.root_dir.numentries-1;
+	fsd.root_dir.entry[i].inode_num = fsd.inodes_used-1;
 	strncpy(fsd.root_dir.entry[i].name, filename, FILENAMELEN);
+	fsd.root_dir.numentries++;
 
-	success = fs_open(filename, O_RDWR);
-	if(success == SYSERR)
-	{
-		printf("Couldn't create file");
-		return SYSERR;
-	}	
-
-
-	return OK;
+	return fs_open(filename, O_RDWR);
+	
 }
 
 int fs_seek(int fd, int offset) {
@@ -376,13 +377,70 @@ int fs_seek(int fd, int offset) {
 	return OK;
 }
 
-int fs_read(int fd, void *buf, int nbytes) {
-  return SYSERR;
+int fs_read(int fd, void *buf, int nbytes) 
+{	
+	int blockindex, blockno, offset, temp, count, success;
+	char *buff = buf;
+	
+	if(fd<0 || fd>= NUM_FD)
+	{
+		printf("Invalid fd\n");
+		return SYSERR;
+	}	
+
+	if(nbytes<1)
+	{
+		printf("Cannot read less than 1 byte\n");
+		return SYSERR;
+	}
+
+	if(oft[fd].state==FSTATE_CLOSED)
+	{
+		printf("Could not read file as it is closed\n");
+		return SYSERR;
+	}
+	
+	blockindex=oft[fd].fileptr/dev0_blocksize;
+	blockno=oft[fd].in.blocks[blockindex];
+	offset=oft[fd].fileptr % dev0_blocksize;
+	
+	count = 0;
+	temp = 0;
+
+	while(nbytes>count)
+	{
+		if(dev0_blocksize - offset > nbytes)
+			temp = nbytes;
+		else
+			temp = dev0_blocksize - offset;
+		
+		success = bs_bread(dev0, blockno , offset, &buff[count], temp);
+		
+		if(success == SYSERR)
+		{
+			printf("Couldn't read block");
+			return SYSERR;
+		}
+		
+		count += temp;	
+		oft[fd].fileptr += temp;
+		nbytes -= temp;
+		blockindex += 1;
+		offset = 0;
+				
+		if(blockno == -1)
+		{
+			printf("Couldn't read block\n");
+			return SYSERR;
+		}			
+	}
+	return count;	
 }
 
 int fs_write(int fd, void *buf, int nbytes) {
-/*		
-
+	int blockindex, offset, blockno, i, success, count, temp;
+	char *buff = buf;
+		
 	if(fd<0 || fd>= NUM_FD)
 	{
 		printf("Invalid fd\n");
@@ -401,13 +459,62 @@ int fs_write(int fd, void *buf, int nbytes) {
 		return SYSERR;
 	}
 
+	oft[fd].in.size = oft[fd].fileptr;
+	blockindex = oft[fd].fileptr/dev0_blocksize;
+	offset = oft[fd].fileptr%dev0_blocksize;
+	blockno = oft[fd].in.blocks[blockindex];
 
-*/
+	if(blockno == -1)
+	{
+		for(i=0; i<dev0_numblocks;i++)
+		{
+			if(fs_getmaskbit(i)==0)
+			{
+				blockno = i;
+				break;
+			}
+		}
+		if(blockno == -1)
+		{
+			printf("No blocks are free");
+			return SYSERR;
+		}
+		oft[fd].in.blocks[blockindex] = blockno;
+	}
 	
+	count = 0;
+	while(nbytes>0)
+	{
+		fs_setmaskbit(blockno);
+		if(dev0_blocksize - offset > nbytes)
+			temp = nbytes;
+		else
+			temp = dev0_blocksize - offset;
 	
+		success = bs_bwrite(dev0, blockno, offset, &buff[count], temp);
+		if(success == SYSERR)
+		{
+			printf("Could not write to block");
+			return SYSERR;
+		}
 	
-	
-  return SYSERR;
+		nbytes -= temp;
+		count += temp;
+		blockindex++;
+		for(i=0; i<dev0_numblocks;i++)
+		{
+			if(fs_getmaskbit(i)==0)
+			{
+				oft[fd].in.blocks[blockindex] = i;
+				break; 
+			}
+		}
+		blockno = oft[fd].in.blocks[blockindex];					
+	}
+	oft[fd].in.size += count;
+	oft[fd].fileptr = oft[fd].in.size;
+ 	return count;
+
 }
 
 #endif /* FS */
